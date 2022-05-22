aa9dd33d5bfd79be272cb39e63c7d1447d3bc487
Younus <younusshamim.bd@gmail.com>
2022-05-22 13:41:21 +0600

    Next Rest API with MongoDB

--- components/PizzaCard.js
+++ components/PizzaCard.js
@@ -1,18 +1,21 @@
 import styles from "../styles/PizzaCard.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
-const PizzaCard = () => {
+const PizzaCard = ({ pizza }) => {
   return (
     <div className={styles.container}>
-      <Image
-        src="/img/pizza.png"
-        height="500"
-        width="500"
-        alt="pizza"
-        className={styles.img}
-      />
-      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
-      <span className={styles.price}>$19.90</span>
+      <Link href={`/product/${pizza._id}`}>
+        <Image
+          src={pizza.img}
+          height="500"
+          width="500"
+          alt="pizza"
+          className={styles.img}
+        />
+      </Link>
+      <h1 className={styles.title}>{pizza.title}</h1>
+      <span className={styles.price}>${pizza.prices[0]}</span>
       <p className={styles.desc}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, quas?
       </p>
